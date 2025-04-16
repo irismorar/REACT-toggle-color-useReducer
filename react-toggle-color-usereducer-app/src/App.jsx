4f8fcaca98f50af0abe6187688bb0c5b405492a0
Iris Adina Morar <morariris@gmail.com>
2025-04-16 17:05:15 +0300

    add complexity to the app

--- react-toggle-color-usereducer-app/src/App.jsx
+++ react-toggle-color-usereducer-app/src/App.jsx
@@ -2,13 +2,22 @@ import { useReducer } from "react";
 import { useEffect } from "react";
 import "./App.css";
 
-const initialTheme = "light";
+const initialState = { theme: "light", font: "small" };
 
-function themeReducerFunction(state, action) {
+function reducerFunction(state, action) {
   const { type } = action;
   switch (type) {
     case "TOGGLE_THEME": {
-      return state === "light" ? "dark" : "light";
+      return {
+        ...state,
+        theme: state.theme === "light" ? "dark" : "light",
+      };
+    }
+    case "TOGGLE_FONT": {
+      return {
+        ...state,
+        font: state.font === "small" ? "large" : "small",
+      };
     }
     default: {
       return state;
@@ -17,29 +26,28 @@ function themeReducerFunction(state, action) {
 }
 
 export default function App() {
-  const [currentTheme, dispatch] = useReducer(
-    themeReducerFunction,
-    initialTheme
-  );
+  const [currentState, dispatch] = useReducer(reducerFunction, initialState);
 
   useEffect(() => {
     document.body.style.backgroundColor =
-      currentTheme === "light" ? "#ccc" : "#222";
-    document.body.style.color = currentTheme === "light" ? "#444" : "#ccc";
-  }, [currentTheme]);
+      currentState.theme === "light" ? "#ccc" : "#222";
+    document.body.style.color =
+      currentState.theme === "light" ? "#444" : "#ccc";
+    document.body.style.fontSize =
+      currentState.font === "small" ? "1rem" : "2rem";
+  }, [currentState]);
 
   return (
     <>
       <header>
-        <h1>Bright or not?</h1>
+        <h1>Toggle or not?</h1>
       </header>
       <main>
-        <div>Current theme: {currentTheme.toUpperCase()}</div>
-        <br />
+        <div>Current theme: {currentState.theme.toUpperCase()}</div>
         <button
           style={{
             border:
-              currentTheme === "light"
+              currentState.theme === "light"
                 ? "1px solid #444"
                 : "1px solid transparent",
           }}
@@ -48,6 +56,22 @@ export default function App() {
           }}
         >
           Toggle app theme
+        </button>
+        <br />
+        <br />
+        <div>Current font: {currentState.font.toUpperCase()}</div>
+        <button
+          style={{
+            border:
+              currentState.theme === "light"
+                ? "1px solid #444"
+                : "1px solid transparent",
+          }}
+          onClick={() => {
+            dispatch({ type: "TOGGLE_FONT" });
+          }}
+        >
+          Toggle app font size
         </button>
       </main>
     </>
